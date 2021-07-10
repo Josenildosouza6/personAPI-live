bc41ab826b605d96771ae340b98a76058eda5dcb
Josenildosouza6 <josenildo.souza6@gmail.com>
2021-07-09 21:50:19 -0300

    add mudanças

--- src/main/java/one/digitalinnovation/personapi/PersonapiApplication.java
+++ src/main/java/one/digitalinnovation/personapi/PersonapiApplication.java
@@ -1,10 +1,8 @@
 package one.digitalinnovation.personapi;
 
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class PersonapiApplication {
